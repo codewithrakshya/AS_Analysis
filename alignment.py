8aad8edacf6d0d95a4cc84955f3190e2c9ad1dca
Rakshya Sharma <73920115+codewithrakshya@users.noreply.github.com>
2023-06-07 14:03:20 -0700

    Update alignment.py

--- alignment.py
+++ alignment.py
@@ -2,7 +2,6 @@ import pysam
 
 def read_bam_file(bam_file_path):
     bamfile = pysam.AlignmentFile(bam_file_path, "rb")
-    # Read and process the BAM file
     return bamfile
 
 def close_bam_file(bamfile):
