7a405aca3917b2418d2633745a59516e3442f1a0
Rakshya Sharma <73920115+codewithrakshya@users.noreply.github.com>
2023-06-07 14:04:33 -0700

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 def calculate_std_deviation(splicing_pattern_frequencies):
-    # Calculate standard deviation of frequencies
+    frequencies = list(splicing_pattern_frequencies.values())
+    std_deviation = np.std(frequencies)
     return std_deviation
+
