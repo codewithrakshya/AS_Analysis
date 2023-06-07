23594e42e3a77c3f8c1118da6f37784679402526
Rakshya Sharma <73920115+codewithrakshya@users.noreply.github.com>
2023-06-07 14:04:54 -0700

    Update output.py

--- output.py
+++ output.py
@@ -1,3 +1,6 @@
-def write_output_to_file(output_data, file_path):
-    # Write output data to a file (TSV or CSV)
-    pass
+import pandas as pd
+
+def write_output_to_file(splicing_pattern_frequencies, output_file_path):
+    df = pd.DataFrame.from_dict(splicing_pattern_frequencies, orient='index', columns=['Frequency'])
+    df.index.name = 'Splicing Pattern'
+    df.to_csv(output_file_path, sep='\t')
