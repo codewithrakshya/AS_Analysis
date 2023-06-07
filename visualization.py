f9bf9a2f6b38f81c2a982a9cd32e271c86177e07
Rakshya Sharma <73920115+codewithrakshya@users.noreply.github.com>
2023-06-07 14:04:14 -0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 
 def plot_splicing_patterns(splicing_pattern_frequencies):
-    # Plot splicing patterns using Matplotlib
-    plt.bar(...)
+    patterns = list(splicing_pattern_frequencies.keys())
+    frequencies = list(splicing_pattern_frequencies.values())
+
+    plt.bar(patterns, frequencies)
+    plt.xlabel('Splicing Pattern')
+    plt.ylabel('Frequency')
+    plt.title('Splicing Pattern Frequencies')
+    plt.xticks(rotation=90)
     plt.show()
