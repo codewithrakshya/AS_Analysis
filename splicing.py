cce968515abd6e67a1a1676d96960bcda78996ee
Rakshya Sharma <73920115+codewithrakshya@users.noreply.github.com>
2023-06-07 14:01:52 -0700

    Create splicing.py

---
+++ splicing.py
@@ -0,0 +1,11 @@
+def detect_alternative_splicing(read):
+    # Detect alternative splicing events
+    return splicing_events
+
+def analyze_cigar_strings(bamfile, max_reads):
+    # Analyze CIGAR strings and count splicing patterns
+    return splicing_patterns
+
+def get_splicing_pattern_frequencies(splicing_patterns):
+    # Calculate frequencies of splicing patterns
+    return splicing_pattern_frequencies
